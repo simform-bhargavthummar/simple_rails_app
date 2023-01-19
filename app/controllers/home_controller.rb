6c0494bf2e30e39d4338f8afd151d2b327313e05
Kishan lad <kishan.l@simformsolution.com>
2023-01-19 11:33:29 +0530

    TE7-T115:rails-practice:Feat:Adding home module

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+      
+      @name = "Kishan"
+      @company_name = "Simform Solution"
+      @collage_name = "Parul University" 
+      @hobbies = ["Reading Books","Learning"]
+      @current_learning = ["Ruby on Rails(ROR)","Hacking"]
+      @current_hacking_framework = "Reverse Engineering"
+      
+  end
+
+end
