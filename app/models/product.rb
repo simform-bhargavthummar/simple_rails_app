66b764beb41ebea9969fedd0e1537a3ff6f38179
Kishan lad <kishan.l@simformsolution.com>
2023-01-23 19:38:32 +0530

    TE7-T115:rails-practice:Feat:Active Record

---
+++ app/models/product.rb
@@ -0,0 +1,19 @@
+class Product < ApplicationRecord
+    validates :company_name , presence: true
+    validates :name , presence: true
+    validates :price , presence: true
+    validates :quantity , presence: true
+    validates :description , presence: true , length: {minimum:3, maximum:200}
+
+
+    before_validation :print_notice
+    after_save :print_status
+    
+    def print_notice
+        puts "Checking data..."
+    end
+
+    def print_status
+        puts "Data added sucessfully."
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root "home#index"
+ 
+  get "/products" , to: 'products#index' , as: "products" 
+  get "/show/:id" , to: 'products#show' , as: "show"
+
+  root "products#index"
   get "hello" => "home#hello"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
