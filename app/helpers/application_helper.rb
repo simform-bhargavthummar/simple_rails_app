ad25aa58ae9e7e0b83a430a407199e3e1be4c9b5
Kishan lad <kishan.l@simformsolution.com>
2023-03-23 18:02:30 +0530

    TE7-T121:rails-practice:Fix:Active Record Query Interface updating and optimizing code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_errors(model)
+    if model.errors.any?
+      content_tag(:div, class: "alert alert-danger") do
+        concat(content_tag(:ul) do
+          model.errors.full_messages.each do |msg|
+            concat content_tag(:li, msg)
+          end
+        end)
+      end
+    end
+  end
 end
